d576fa8241df384654e22eb7bba48f54457cf99d
Glenn <hazelnutvt04@gmail.com>
2020-09-25 00:10:15 -0400

    Split implementations

---
+++ include/cookbook_fft.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <cmath>
+#include <complex>
+#include <iostream>
+#include <iterator>
+
+#include "my_utils.hpp"
+
+// FFT Implementation from C++ Cookbook:
+// https://www.oreilly.com/library/view/c-cookbook/0596007612/ch11s18.html#cplusplusckbk-CHP-11-EX-33
+
+unsigned int bit_reverse(unsigned int x, int log2n) {
+   int n = 0;
+   for (int i = 0; i < log2n; i++) {
+      n <<= 1;
+      n |= (x & 1);
+      x >>= 1;
+   }
+   return n;
+}
+
+constexpr double PI = 3.1415926536;
+
+template <class Iter_T>
+void cookbook_fft(Iter_T a, Iter_T b, int log2n) {
+
+   typedef typename std::iterator_traits<Iter_T>::value_type complex;
+   const complex J(0, 1);
+   int fft_size = 1 << log2n;
+   
+   std::cout << "Number of bits in FFT Size is " << log2n << "\n"; 
+   std::cout << "FFT Size is " << fft_size << "\n"; 
+
+   for (unsigned int i = 0; i < fft_size; ++i) {
+      unsigned int br_index = bit_reverse(i, log2n);
+      std::cout << "\t" << i << " bit reversed is " << br_index << "\n"; 
+      b[br_index] = a[i];
+   }
+
+   for (int s = 1; s <= log2n; ++s) {
+      int m = 1 << s;
+      int m2 = m >> 1;
+
+      std::cout << "\ts = " << s << " m is " << m << "\n"; 
+      std::cout << "\ts = " << s << " m2 is " << m2 << "\n"; 
+      complex w(1, 0);
+      complex wm = exp(-J * (PI / m2));
+      std::cout << "\ts = " << s << " wm, the exponent, is " << wm << "\n"; 
+      for (int j = 0; j < m2; ++j) {
+         std::cout << "\ts is " << s << " and j is " << j << "\n"; 
+         for (int k = j; k < fft_size; k += m) {
+            std::cout << "\t\ts = " << s << " and j is " << j << " and k, one index into b, is " << k << "\n"; 
+            std::cout << "\t\ts = " << s << " and j is " << j << " and m2 is " << m2 << "\n"; 
+            std::cout << "\t\ts = " << s << " and j is " << j << " and k + m2, the other index into b, is " 
+               << (k + m2) << "\n"; 
+            complex t = w * b[k + m2];
+            complex u = b[k];
+            b[k] = u + t;
+            b[k + m2] = u - t;
+            std::cout << "\t\ts = " << s << " and j is " << j <<  " and b[k] = " << b[k] << "\n";
+            std::cout << "\t\ts = " << s << " and j is " << j <<  " and b[k + m2] = " << b[k + m2] 
+               << "\n";
+         }
+         w *= wm;
+         std::cout << "\ts = " << s << " j = " << j << ": w = " << w << "\n"; 
+      }
+   }
+}
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,26 @@
 
 #include "my_utils.hpp"
 
-#include "my_fft.hpp"
+#include "cookbook_fft.hpp"
+#include "ct_fft.hpp"
 
 int main(int argc, char** argv) {
    try {
       bool debug = true;
 
       typedef std::complex<double> cx;
-      cx a[] = {cx(0, 0), cx(1, 1), cx(3, 3), cx(4, 4), cx(4, 4), cx(3, 3), cx(1, 1), cx(0, 0)};
+      cx a[] = {cx(0, 0), cx(1, 1), cx(3, 3), cx(4, 4), 
+         cx(4, 4), cx(3, 3), cx(1, 1), cx(0, 0)};
       cx b[8];
-      my_fft(a, b, 3);
+      cookbook_fft(a, b, 3);
       for (int i = 0; i < 8; ++i)
          std::cout << b[i] << "\n";
 
-      double ct_a[] = {0, 0, 1, 1, 3, 3, 4, 4, 4, 4, 3, 3, 1, 1, 0, 0};
-      ct_fft(ct_a, 8);
-      for (int i = 0; i < 16; ++i)
-         std::cout << ct_a[i] << "\n";
+      // double ct_a[] = {0, 0, 1, 1, 3, 3, 4, 4,
+         // 4, 4, 3, 3, 1, 1, 0, 0};
+      // ct_fft(ct_a, 8);
+      // for (int i = 0; i < 16; ++i)
+         // std::cout << ct_a[i] << "\n";
 
       return EXIT_SUCCESS;
 
